56bf22b81c2bc23e60b89bf14b486d4399e2980d
CEDBRASIL <cedbrasil@outlook.com>
2025-06-02 23:43:47 +0000

    dadada

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -19,9 +20,7 @@ def webhook():
 @app.route('/')
 def home():
     return "Servidor ativo. Endpoint de webhook: /webhook", 200
-    
-    if __name__ == '__main__':
-    import os
+
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-
